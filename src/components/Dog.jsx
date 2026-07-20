8df0fcb5ba7c38c608da992b278dc4953f0f5a71
manthansharma17 <manthannaruto10@gmail.com>
2026-07-20 22:47:09 +0530

    imports fix

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useRef, useMemo } from "react";
 import * as THREE from "three";
-import { Canvas, useThree } from "@react-three/fiber";
-import {
-  OrbitControls,
-  useGLTF,
-  useTexture,
-  useAnimations,
-} from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { useGLTF, useTexture, useAnimations } from "@react-three/drei";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
